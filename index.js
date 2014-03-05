11111bf5a204b4c554a0498379d43d6d7c7df4ca
mgechev <mgechev@gmail.com>
2014-03-05 13:27:54 +0200

    Add command line parameters

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var contributors = require('./lib/contributors'),
     tablePhotos = require('./lib/strategies/table-photos');
 
+tablePhotos.setCols(5);
+
 contributors.formatStrategy = tablePhotos;
 
-contributors.get('mgechev', 'angularjs-style-guide')
+contributors.get(process.argv[2], process.argv[3])
 .then(function (obj) {
   console.log(obj);
 });
