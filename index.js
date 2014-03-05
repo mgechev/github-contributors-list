6a762c7c9ed387577ef59888187a8eb03f8284b9
mgechev <mgechev@gmail.com>
2014-03-05 12:20:18 +0200

    Add strategy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var contributors = require('./lib/contributors'),
     tablePhotos = require('./lib/strategies/table-photos');
 
+contributors.formatStrategy = tablePhotos;
+
 contributors.get('mgechev', 'angularjs-style-guide')
 .then(function (obj) {
   console.log(obj);
