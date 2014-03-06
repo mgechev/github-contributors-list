941f1f4a084ef28214ef73d1dda1064a3d1ec2ba
mgechev <mgechev@gmail.com>
2014-03-06 12:09:06 +0200

    Fix the list strategy

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var args = require('minimalist')(process.argv.slice(2)),
+var args = require('minimist')(process.argv.slice(2)),
     contributors = require('./lib/contributors'),
     table = require('./lib/strategies/table'),
     list = require('./lib/strategies/list'),
@@ -7,11 +7,18 @@ var args = require('minimalist')(process.argv.slice(2)),
       list: list
     };
 
+if (args.layout) {
+  if (!strategies[args.layout]) {
+    throw new Error('Invalid layout strategy', args.layout);
+  }
+} else {
+  args.layout = 'table';
+}
 
+args.strategy = strategies[args.layout](args);
+contributors = contributors(args);
 
-contributors.formatStrategy = tablePhotos;
-
-contributors.get(process.argv[2], process.argv[3])
+contributors.get()
 .then(function (obj) {
   console.log(obj);
 });
