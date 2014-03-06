85e15402e420fc6024a0d713e2fb6526046dac37
mgechev <mgechev@gmail.com>
2014-03-06 11:56:10 +0200

    Refactor strategies

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var contributors = require('./lib/contributors'),
-    tablePhotos = require('./lib/strategies/table-photos');
+var args = require('minimalist')(process.argv.slice(2)),
+    contributors = require('./lib/contributors'),
+    table = require('./lib/strategies/table'),
+    list = require('./lib/strategies/list'),
+    strategies = {
+      table: table,
+      list: list
+    };
+
+
 
 contributors.formatStrategy = tablePhotos;
 
